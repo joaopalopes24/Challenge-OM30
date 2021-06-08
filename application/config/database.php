96a712dd232565c4f5c8b2b7b6dbddceb7bee95e
joaopalopes24 <joaopalopes24@gmail.com>
2021-06-08 17:24:02 -0300

    feat(migrations): criaçao da Migration de Pacientes que sera utilizada no Sistema

--- application/config/database.php
+++ application/config/database.php
@@ -75,11 +75,11 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
-	'dbdriver' => 'mysqli',
+	'hostname' => 'desafio-postgres',
+	'username' => 'desafio',
+	'password' => '123456',
+	'database' => 'desafio-ci',
+	'dbdriver' => 'postgre',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
---
+++ application/migrations/001_patients.php
@@ -0,0 +1,87 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_patients extends CI_Migration {
+
+  public function up()
+    {$this->dbforge->add_field(array(
+     'id' => array(
+        'type' => 'INT',
+          'constraint' => 11,
+          'unsigned' => TRUE,
+          'auto_increment' => TRUE
+      ),
+      'photo' => array(
+        'type' => 'VARCHAR',
+        'constraint' => '255',
+        'null' => TRUE,
+      ),
+      'full_name' => array(
+        'type' => 'VARCHAR',
+        'constraint' => '255',
+      ),
+      'birthday' => array(
+        'type' => 'DATE',
+      ),
+      'cpf' => array(
+        'type' => 'VARCHAR',
+        'constraint' => '14',
+      ),
+      'cns' => array(
+        'type' => 'VARCHAR',
+        'constraint' => '15',
+      ),
+      'cep' => array(
+        'type' => 'VARCHAR',
+        'constraint' => '9',
+      ),
+      'adress' => array(
+        'type' => 'VARCHAR',
+        'constraint' => '50',
+      ),
+      'number' => array(
+        'type' => 'INT',
+        'constraint' => '5',
+        'unsigned' => TRUE,
+      ),
+      'complement' => array(
+        'type' => 'VARCHAR',
+        'constraint' => '30',
+        'null' => TRUE,
+      ),
+      'district' => array(
+        'type' => 'VARCHAR',
+        'constraint' => '40',
+      ),
+      'city' => array(
+        'type' => 'VARCHAR',
+        'constraint' => '40',
+      ),
+      'state_abbr' => array(
+        'type' => 'VARCHAR',
+        'constraint' => '2',
+      ),
+      'created_at' => array(
+        'type' => 'TIMESTAMP',
+        'null' => TRUE,
+      ),
+      'updated_at' => array(
+        'type' => 'TIMESTAMP',
+        'null' => TRUE,
+      ),
+      'deleted_at' => array(
+        'type' => 'TIMESTAMP',
+        'null' => TRUE,
+      ),
+    ));
+    $this->dbforge->add_key('id', TRUE);
+    $this->dbforge->create_table('patients');
+    }
+
+    public function down()
+    {
+     $this->dbforge->drop_table('patients');
+    }
+
+}       
